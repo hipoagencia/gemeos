0ccd687b190b3d8bb4e6c91a448c2c72264b724c
hipoagencia <falecom@hipoagencia.com.br>
2022-01-27 15:32:39 -0300

    Estoque atualiza de 15 em 15

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('sitemap:generate')->daily();
-        $schedule->command('get:stock')->everyMinute();
+        $schedule->command('get:stock')->everyFifteenMinutes();
 
         $schedule->command('backup:clean')->daily()->at('01:00');
         $schedule->command('backup:run')->daily()->at('02:00');
