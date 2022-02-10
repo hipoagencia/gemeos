5c14ba0278b5e00a4ea00778b431bc93d24b6fb8
hipoagencia <falecom@hipoagencia.com.br>
2022-02-10 16:34:46 -0300

    ajustes

--- resources/views/web/impressao.blade.php
+++ resources/views/web/impressao.blade.php
@@ -8,18 +8,18 @@
 </head>
 <body>
 
-<div style="text-align: center;padding-top:80px;">
+<div style="text-align: center;padding-top:20px;">
 
     <img src="{{asset(url('web/assets/img/logo_constru.png'))}}" width="150" style="margin-bottom:20px;"/>
 
-    <h1 style="font-size:60px">{{$car->marca}} {{$car->modelo}}<br>{{$car->versao}}</h1>
-    <h2  style="font-size:50px">{{$car->anofabricacao}} / {{$car->anomodelo}}</h2>
-    <h2  style="font-size:40px">{{$car->km}} km</h2>
+    <h1 style="font-size:50px">{{$car->marca}} {{$car->modelo}}<br>{{$car->versao}}</h1>
+    <h2  style="font-size:40px">{{$car->anofabricacao}} / {{$car->anomodelo}}</h2>
+    <h2  style="font-size:35px">{{$car->km}} km</h2>
 
     <img alt="Carro"  src="{{$car->cover->url}}" width="400">
 
 
-    <ul class="car-list" style="width: 50%; margin:0px auto; padding-top:70px;padding-bottom:70px;">
+    <ul class="car-list" style="width: 50%; margin:0px auto; padding-top:40px;padding-bottom:40px;">
         @foreach($opcinals as $opcinal)
             <li>{{$opcinal}}</li>
         @endforeach
